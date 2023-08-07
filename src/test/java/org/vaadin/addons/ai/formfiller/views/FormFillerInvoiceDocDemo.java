8f1a0468aa21670d5413955636298f3d380b7368
Peter Czuczor <czuczor@gmail.com>
2023-08-07 09:50:01 +0200

    Fixed some invoices (invoice5, invoice hu) name/extension

--- src/test/java/org/vaadin/addons/ai/formfiller/views/FormFillerInvoiceDocDemo.java
+++ src/test/java/org/vaadin/addons/ai/formfiller/views/FormFillerInvoiceDocDemo.java
@@ -139,7 +139,7 @@ public class FormFillerInvoiceDocDemo extends Div {
         imageFile.setReceiver(fileBuffer);
 
         ComboBox<String> images = new ComboBox<>("Select Image");
-        images.setItems("Load my invoice...", "Invoice1", "Invoice2", "Invoice3", "Invoice4", "Invoice5", "Invoice_HU_1.png");
+        images.setItems("Load my invoice...", "Invoice1", "Invoice2", "Invoice3", "Invoice4", "Invoice5", "Invoice_HU_1");
         images.setValue("Load my invoice...");
         images.setAllowCustomValue(false);
         images.addValueChangeListener(e -> {
